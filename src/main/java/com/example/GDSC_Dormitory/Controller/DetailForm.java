4e586172017ab047ccb44959029411ea8feb00e6
bak3839 <bak3839@naver.com>
2023-08-13 23:08:43 +0900

    add DetailForm

---
+++ src/main/java/com/example/GDSC_Dormitory/Controller/DetailForm.java
@@ -0,0 +1,17 @@
+package com.example.GDSC_Dormitory.Controller;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter
+public class DetailForm {
+    private String nickname;
+    private String habits;
+    private String outing;
+    private String prefer;
+    private int sleep;
+    private int wakeup;
+    private int cleaning;
+    private boolean exp;
+    private boolean smoking;
+}
--- src/main/java/com/example/GDSC_Dormitory/service/DetailService.java
+++ src/main/java/com/example/GDSC_Dormitory/service/DetailService.java
@@ -1,4 +1,14 @@
 package com.example.GDSC_Dormitory.service;
 
+import com.example.GDSC_Dormitory.repository.DetailRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
 public class DetailService {
+
+    private final DetailRepository detailRepository;
 }
